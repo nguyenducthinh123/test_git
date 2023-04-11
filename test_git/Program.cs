d1cd7cffcbca8e4f958f12e1b16759678178c8a2
Nguyễn Đức Thịnh <thinhnd111222@gmail.com>
2023-04-11 14:27:35 +0700

    Add project files.

---
+++ test_git/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello Tri");
+            Console.ReadKey();
+        }
+    }
+}
